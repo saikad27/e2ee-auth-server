3d0797980aebf7e28de274612693c129890bb1ba
saikad27 <sairajkad501@icloud.com>
2026-04-15 15:27:21 +0530

    first commit

---
+++ src/main/java/com/example/e2ee_auth_server/E2eeAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.e2ee_auth_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2eeAuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E2eeAuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/e2ee_auth_server/E2eeAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.e2ee_auth_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E2eeAuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
